71811200f347f6a9ec99b2c3970fbb2f45e04e8f
etsai <etsai2@gmail.com>
2013-08-28 22:54:21 -0700

    Modified data table to generate a table anyways for non existant records

--- http/groovy/DataHtml.groovy
+++ http/groovy/DataHtml.groovy
@@ -52,59 +52,57 @@ public class DataHtml extends Resource {
             case "profile":
                 def steamid64= queries.steamid64
                 def row= reader.getRecord(steamid64);
+                def keys= [wins: "Wins", losses: "Losses", disconnects: "Disconnects", finales_played: "Finales Played", 
+                    finales_survived: "Finales Survived", time: "Time Connected"]
+                def stats= [:]
+                def steamIdInfo
 
                 if (row == null) {
-                    xml.center("No records found for SteamID64: " + steamid64)
+                    keys.each {key, title ->
+                        stats[key]= "---"
+                    }
+                    steamIdInfo= [name: "No record for $steamid64", 
+                        avatar: "http://media.steampowered.com/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg"]
                 } else {
-                    def steamIdInfo= reader.getSteamIDInfo(steamid64)
-                    xml.table(tableAttr) {
-                        thead() {
-                            tr() {
-                                th(colspan: "3") {
-                                    h2(steamIdInfo.name)
-                                }
+                    keys.each {key, title ->
+                        stats[key]= row[key]
+                    }
+                    steamIdInfo= reader.getSteamIDInfo(steamid64)
+                }
+                xml.table(tableAttr) {
+                    thead() {
+                        tr() {
+                            th(colspan: "3") {
+                                h2(steamIdInfo.name)
                             }
                         }
-                        tbody() {
-                            tr(class: "odd-row") {
-                                td("Wins")
-                                td(row.wins)
-                                td(rowspan: "7", align: "center") {
-                                    img(src: steamIdInfo.avatar)
+                    }
+                    xml.tbody() {
+                        def even= false, first= true
+                        stats.each {stat, value ->
+                            xml.tr(class: (even ? "even-row" : "odd-row")) {
+                                td(keys[stat])
+                                td(stat.contains("time") ? (row == null ? "---" : Time.secToStr(value)) : value)
+                                if (first) {
+                                    td(rowspan: "7", align: "center") {
+                                        img(src: steamIdInfo.avatar)
+                                    }
+                                    first= false
                                 }
                             }
-                            tr(class: "even-row") {
-                                td("Losses")
-                                td(row.losses)
-                            }
-                            tr(class: "odd-row") {
-                                td("Disconnects")
-                                td(row.disconnects)
-                            }
-                            tr(class: "even-row") {
-                                td("Finales Played")
-                                td(row.finales_played)
-                            }
-                            tr(class: "odd-row") {
-                                td("Finales Survived")
-                                td(row.finales_survived)
-                            }
-                            tr(class: "even-row") {
-                                td("Time Connected")
-                                td(Time.secToStr(row.time))
-                            }
-                            tr(class: "odd-row") {
-                                td(colspan: "2") {
-                                    ul(class: "nav-list") {
-                                        li(class: "nav-list") {
-                                            a(target: "_blank", href: "http://steamcommunity.com/profiles/" + steamid64, "Steam Community Page")
-                                        }
-                                        li(class: "nav-list") {
-                                            a(target: "_blank", href: "matchhistory.html?steamid64=" + steamid64, "Match History")
-                                        }
-                                        li(class: "nav-list") {
-                                            a(href: 'javascript:showPerkLevel(' + steamid64 + ')', "Perk Progression")
-                                        }
+                            even= !even
+                        }
+                        tr(class: (even ? "even-row" : "odd-row")) {
+                            td(colspan: "2") {
+                                ul(class: "nav-list") {
+                                    li(class: "nav-list") {
+                                        a(target: "_blank", href: "http://steamcommunity.com/profiles/$steamid64", "Steam Community Page")
+                                    }
+                                    li(class: "nav-list") {
+                                        a(target: "_blank", href: "matchhistory.html?steamid64=$steamid64", "Match History")
+                                    }
+                                    li(class: "nav-list") {
+                                        a(href: "javascript:showPerkLevel('$steamid64')", "Perk Progression")
                                     }
                                 }
                             }
